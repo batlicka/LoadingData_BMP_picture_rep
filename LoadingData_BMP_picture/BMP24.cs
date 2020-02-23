b1d21109bc1f98edf4caa357e90d954605a9124d
batlicka <46314552+batlicka@users.noreply.github.com>
2020-02-23 22:02:12 +0100

    basic commit

---
+++ LoadingData_BMP_picture/BMP24.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadingData_BMP_picture
+{
+    class BMP24
+    {
+        public byte[] buff;
+
+        public BMP24(byte[] intBuff)
+        {
+            this.buff = intBuff;
+        }
+        public Int32 GiveFirst() {
+            return buff[0];
+        }
+    }
+}
---
+++ LoadingData_BMP_picture/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LoadingData_BMP_picture
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            String fileName = "d:\\dokumenty\\Vojta\\UTB\\visual_studio_repos\\picturesSources\\jednoduchy1.bmp";
+            FileStream fs = new FileStream("d:\\dokumenty\\Vojta\\UTB\\visual_studio_repos\\picturesSources\\jednoduchy1.bmp", FileMode.Open,FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            long numBytes = new FileInfo(fileName).Length;
+            byte[] buff;
+            buff = new byte[(int)numBytes];
+            buff = br.ReadBytes((int)numBytes);
+
+            BMP24 ArrayPic = new BMP24(buff);
+            //vypíše první byte souboru na pozici [0]
+            Console.Write(ArrayPic.GiveFirst());
+            
+            //FileToByteArray("d:\\dokumenty\\Vojta\\UTB\\visual_studio_repos\\picturesSources\\jednoduchy1.bmp");
+        }
+    }    
+}
